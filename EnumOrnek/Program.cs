ca88c098bcb3ad8d9442d6d33f7deed9d8558727
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-14 20:20:51 +0300

    Proje dosyası ekle.

---
+++ EnumOrnek/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumOrnek
+{
+    class Program
+    {
+        enum gunler
+        {
+            Pazartesi=1,
+            Salı=2,
+            Çarşamba=3,
+            Perşembe=4,
+            Cuma=5,
+            Cumartesi=6,
+            Pazar=7
+        }
+        static void Main(string[] args)
+        {
+            int secilengun = (int)gunler.Cuma;
+            if (secilengun==(int)gunler.Cumartesi || secilengun==(int)gunler.Pazar)
+            {
+                Console.WriteLine("Hafta sonudur.");
+            }
+            else
+            {
+                Console.WriteLine("Hafta içidir");
+            }
+            Console.ReadLine();
+            }
+        }
+    }
